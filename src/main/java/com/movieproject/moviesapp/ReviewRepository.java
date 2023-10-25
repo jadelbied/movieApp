b16ed0cc23f96693b1e41da37fa5a0cc9f74cf0d
jadelbied <jadelbied@gmail.com>
2023-10-26 00:12:28 +0100

    Finishing the Backend

--- src/main/java/com/movieproject/moviesapp/ReviewRepository.java
+++ src/main/java/com/movieproject/moviesapp/ReviewRepository.java
@@ -1,2 +1,10 @@
-package com.movieproject.moviesapp;public interface ReviewRepository {
+package com.movieproject.moviesapp;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+
 }
